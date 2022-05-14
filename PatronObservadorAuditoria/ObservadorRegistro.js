4f18d83ee9a4090398295be97f5c016d4680ebeb
Christian Jose <christianjsanchezr2025@gmail.com>
2022-05-13 20:41:39 -0400

    Modificado los .js del modulo Patron Observador Auditoria

--- PatronObservadorAuditoria/ObservadorRegistro.js
+++ PatronObservadorAuditoria/ObservadorRegistro.js
@@ -1,30 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ObservableAuditoria = exports.Registro_Actividad = exports.Registro_Auditoria = void 0;
-var Registro_Auditoria = /** @class */ (function () {
-    function Registro_Auditoria() {
+export class Registro_Auditoria {
+    constructor() {
         this.registro = new Array();
     }
-    Registro_Auditoria.prototype.registrar = function (registroA) {
+    registrar(registroA) {
         this.registro.push(registroA);
-    };
-    return Registro_Auditoria;
-}());
-exports.Registro_Auditoria = Registro_Auditoria;
-var Registro_Actividad = /** @class */ (function () {
-    function Registro_Actividad() {
+    }
+}
+export class Registro_Actividad {
+    constructor() {
         this.registro = new Array();
     }
-    Registro_Actividad.prototype.registrar = function (registroA) {
+    registrar(registroA) {
         this.registro.push(registroA);
-    };
-    return Registro_Actividad;
-}());
-exports.Registro_Actividad = Registro_Actividad;
-var ObservableAuditoria = /** @class */ (function () {
-    function ObservableAuditoria(observador) {
+    }
+}
+export class ObservableAuditoria {
+    constructor(observador) {
         this.observador = observador;
     }
-    return ObservableAuditoria;
-}());
-exports.ObservableAuditoria = ObservableAuditoria;
+}
