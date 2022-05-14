4842573e4805bff9564ef8d015813014a6bc71ab
Christian Jose <christianjsanchezr2025@gmail.com>
2022-05-13 20:40:32 -0400

    Modificado los .js del modulo de Suscripciones

--- ModuloSuscripccion/MetodoPago.js
+++ ModuloSuscripccion/MetodoPago.js
@@ -1,69 +1,41 @@
-"use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TDC = exports.Paypal = exports.MetodoPago = exports.TipoPlan = void 0;
-var TipoPlan;
+export var TipoPlan;
 (function (TipoPlan) {
     TipoPlan["mensual"] = "MENSUAL";
     TipoPlan["anual"] = "ANUAL";
-})(TipoPlan = exports.TipoPlan || (exports.TipoPlan = {}));
+})(TipoPlan || (TipoPlan = {}));
 // CREACION DE LAS CLASES DE METODOS DE PAGO
-var MetodoPago = /** @class */ (function () {
-    function MetodoPago(modalidad) {
+export class MetodoPago {
+    constructor(modalidad) {
         this.modalidad = modalidad;
     }
-    return MetodoPago;
-}());
-exports.MetodoPago = MetodoPago;
+}
 //CREACION CLASE PAYPAL
-var Paypal = /** @class */ (function (_super) {
-    __extends(Paypal, _super);
-    function Paypal(email, modalidad) {
-        var _this = _super.call(this, modalidad) || this;
-        _this.email = email;
-        return _this;
+export class Paypal extends MetodoPago {
+    constructor(email, modalidad) {
+        super(modalidad);
+        this.email = email;
     }
-    Paypal.prototype.realizarPago = function (IVisitor) {
+    realizarPago(IVisitor) {
         IVisitor.visitPAYPAL(this);
-    };
-    Paypal.prototype.getName = function () {
-        return "Paypal";
-    };
-    return Paypal;
-}(MetodoPago));
-exports.Paypal = Paypal;
-//CREACION CLASE TDC
-var TDC = /** @class */ (function (_super) {
-    __extends(TDC, _super);
-    function TDC(nombre, banco, f_vencimiento, nro_tarjeta, cvc, modalidad) {
-        var _this = _super.call(this, modalidad) || this;
-        _this.nombre = nombre;
-        _this.banco = banco;
-        _this.f_vencimiento = f_vencimiento;
-        _this.nro_tarjeta = nro_tarjeta;
-        _this.cvc = cvc;
-        return _this;
     }
-    TDC.prototype.realizarPago = function (IVisitor) {
+    getName() {
+        return "Paypal";
+    }
+}
+//CREACION CLASE TDC
+export class TDC extends MetodoPago {
+    constructor(nombre, banco, f_vencimiento, nro_tarjeta, cvc, modalidad) {
+        super(modalidad);
+        this.nombre = nombre;
+        this.banco = banco;
+        this.f_vencimiento = f_vencimiento;
+        this.nro_tarjeta = nro_tarjeta;
+        this.cvc = cvc;
+    }
+    realizarPago(IVisitor) {
         IVisitor.visitTDC(this);
-    };
-    TDC.prototype.getName = function () {
+    }
+    getName() {
         return "TDC";
-    };
-    return TDC;
-}(MetodoPago));
-exports.TDC = TDC;
+    }
+}
--- ModuloSuscripccion/Suscripccion.js
+++ ModuloSuscripccion/Suscripccion.js
@@ -1,40 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Suscripcion = exports.StatusSuscripccion = void 0;
-var MetodoPago_1 = require("./MetodoPago");
-var StatusSuscripccion;
+import { TipoPlan } from "./MetodoPago";
+export var StatusSuscripccion;
 (function (StatusSuscripccion) {
     StatusSuscripccion["Activa"] = "ACTIVA";
     StatusSuscripccion["Cancelada"] = "CANCELADA";
     StatusSuscripccion["Bloqueada"] = "BLOQUEADA";
-})(StatusSuscripccion = exports.StatusSuscripccion || (exports.StatusSuscripccion = {}));
-var Suscripcion = /** @class */ (function () {
-    function Suscripcion() {
-    }
-    Suscripcion.prototype.actualizarFecha = function () {
-        if (this.mpago.modalidad == MetodoPago_1.TipoPlan.mensual) {
+})(StatusSuscripccion || (StatusSuscripccion = {}));
+export class Suscripcion {
+    actualizarFecha() {
+        if (this.mpago.modalidad == TipoPlan.mensual) {
             console.log("MENSUAL");
             this.fecha_inicio = new Date(Date.now());
             this.fecha_fin = new Date(this.fecha_inicio.getFullYear(), this.fecha_inicio.getMonth() + 1, this.fecha_inicio.getDate());
         }
-        if (this.mpago.modalidad == MetodoPago_1.TipoPlan.anual) {
+        if (this.mpago.modalidad == TipoPlan.anual) {
             console.log("ANUAL");
             this.fecha_inicio = new Date(Date.now());
             this.fecha_fin = new Date(this.fecha_inicio.getFullYear() + 1, this.fecha_inicio.getMonth(), this.fecha_inicio.getDate());
         }
-    };
-    Suscripcion.prototype.agregarMetodo = function (mpago) {
+    }
+    agregarMetodo(mpago) {
         this.mpago = mpago;
-    };
-    Suscripcion.prototype.mostrarSuscripccion = function () {
-        console.log("Su Suscripccion Inicio el dia: ".concat(this.fecha_inicio.toLocaleString()));
-        console.log("Su Suscripccion finaliza el dia: ".concat(this.fecha_fin.toLocaleString()));
-        console.log("Su Metodo de Pago fue: ".concat(this.mpago.getName()));
-        console.log("Su Estatus de suscripccion es : ".concat(this.status));
-    };
-    Suscripcion.prototype.actualizarStatus = function (statusSus) {
+    }
+    mostrarSuscripccion() {
+        console.log(`Su Suscripccion Inicio el dia: ${this.fecha_inicio.toLocaleString()}`);
+        console.log(`Su Suscripccion finaliza el dia: ${this.fecha_fin.toLocaleString()}`);
+        console.log(`Su Metodo de Pago fue: ${this.mpago.getName()}`);
+        console.log(`Su Estatus de suscripccion es : ${this.status}`);
+    }
+    actualizarStatus(statusSus) {
         this.status = statusSus;
-    };
-    return Suscripcion;
-}());
-exports.Suscripcion = Suscripcion;
+    }
+}
